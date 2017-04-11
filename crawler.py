8f94afece9efb585e015db1a7a125a22e9d31cf3
garynil <garynil1635@gmail.com>
2017-04-11 20:16:20 +0800

    Basic structure

--- crawler.py
+++ crawler.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
 import os
 import re
 import json
@@ -12,22 +11,67 @@ import urlparse
 import datetime,random
 import UserAgent
 
-# setup some arguments
-# 1. config file path : default in same folder as code
-# 2. poll interval time : default is 780 sec
-def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-c', '--config',
-                        default='%s/config.json' % os.path.dirname(
-                            os.path.realpath(__file__)),
-                        help='Set your path of config.json')
+from copy import copy
+from lxml import html
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from datetime import date, datetime, timedelta
 
-    parser.add_argument('-t', '--poll-interval', type=int, default=780,
-                        help='Time in seconds between price checking')
-    return parser.parse_args()
+ua = UserAgent.UserAgent()
+intervalTimeBetweenCheck = 0
 
-
-# read config file and load setting..
+# read config json from path
 def get_config(config):
     with open(config, 'r') as f:
         return json.loads(f.read())
+
+# add some arguments 
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--config',
+                        default='%s/config.json' % os.path.dirname(os.path.realpath(__file__)),
+                        help='Add your config.json path')
+    parser.add_argument('-t', '--poll-interval', type=int, default=780,
+                        help='Time(second) between checking, default is 780 s.')
+
+    return parser.parse_args()
+
+def main():
+    #set up arguments
+    args = parse_args()
+    intervalTimeBetweenCheck = args.poll_interval
+    
+    # get config from path
+    config = get_config(args.config)
+
+    #get all items to parse
+    items = config['item-to-parse']
+
+    while True and len(items):
+        nowtime = datetime.now()
+        nowtime_Str = nowtime.strftime('%Y-%m-%d %H:%M:%S')
+        print ('[%s] Start Checking' % (nowtime_Str))
+
+        itemIndex = 1
+        for item in copy(items):
+            # url to parse
+            item_page_url = urlparse.urljoin(config['base_url'], item[0])
+            print('[#%02d] Checking price for %s (target price: %s)' % ( itemIndex, item[0], item[1]))
+
+            itemIndex += 1
+
+        if len(items):
+            # time interval add some random number for preventing banning
+            nowtime = datetime.now()
+            thisIntervalTime = intervalTimeBetweenCheck + random.randint(0,150)
+
+            #calculate next triggered time
+            dt = datetime.now() + timedelta(seconds=thisIntervalTime)
+            print('Sleeping for %d seconds, next start at %s' % (thisIntervalTime, dt.strftime('%Y-%m-%d %H:%M:%S')))
+            time.sleep(thisIntervalTime)
+        else:
+            break
+
+
+if __name__ == '__main__':
+    main()
